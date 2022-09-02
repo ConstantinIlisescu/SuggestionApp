0cd8956daa4ade997c07acac51877dd6ba9ffb36
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-09-02 19:59:38 +0100

    Remove white space

--- SugestionAppLibrary/Models/CategoryModel.cs
+++ SugestionAppLibrary/Models/CategoryModel.cs
@@ -4,8 +4,6 @@ public class CategoryModel
    [BsonId]
    [BsonRepresentation(BsonType.ObjectId)]
    public string Id { get; set; }
-
    public string CategoryName { get; set; }
-
    public string CategoryDescription { get; set; }
 }
--- SugestionAppLibrary/Models/StatusModel.cs
+++ SugestionAppLibrary/Models/StatusModel.cs
@@ -4,8 +4,6 @@ public class StatusModel
    [BsonId]
    [BsonRepresentation(BsonType.ObjectId)]
    public string Id { get; set; }
-
    public string StatusName { get; set; }
-
    public string StatusDescription { get; set; }
 }
