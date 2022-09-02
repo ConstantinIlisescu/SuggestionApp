bab16d27ab8bfc22257b1d95059ac96744eb3c61
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-09-02 20:05:12 +0100

    Create an overload constructor

--- SugestionAppLibrary/Models/BasicSuggestionModel.cs
+++ SugestionAppLibrary/Models/BasicSuggestionModel.cs
@@ -3,5 +3,17 @@ public class BasicSuggestionModel
 {
    [BsonRepresentation(BsonType.ObjectId)]
    public string Id { get; set; }
-   public string Title { get; set; }
+   public string Suggestion { get; set; }
+
+   public BasicSuggestionModel()
+   {
+
+   }
+
+   public BasicSuggestionModel(SuggestionModel suggestion)
+   {
+      Id = suggestion.Id;
+      Suggestion = suggestion.Suggestion;
+   }
+
 }
