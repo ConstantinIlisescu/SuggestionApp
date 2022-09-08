1ab0981e46f093491488de592fe2c95b24bf470c
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-09-08 15:00:09 +0100

    Add fields and assign them

--- SugestionAppLibrary/DataAccess/MongoSuggestionData.cs
+++ SugestionAppLibrary/DataAccess/MongoSuggestionData.cs
@@ -3,8 +3,14 @@
 namespace SuggestionAppLibrary.DataAccess;
 public class MongoSuggestionData
 {
+	private readonly IDbConnection _db;
+	private readonly IUserData _userData;
+	private readonly IMemoryCache _cache;
+
 	public MongoSuggestionData(IDbConnection db, IUserData userData, IMemoryCache cache)
 	{
-
+		_db = db;
+		_userData = userData;
+		_cache = cache;
 	}
 }
