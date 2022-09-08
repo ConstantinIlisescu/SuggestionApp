52dadb96b08783d7180ca5287871ca372a773f2a
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-09-08 14:30:39 +0100

    Get a list of categories and cache it in memory for 1 day

--- SugestionAppLibrary/DataAccess/MongoCategoryData.cs
+++ SugestionAppLibrary/DataAccess/MongoCategoryData.cs
@@ -6,10 +6,24 @@ public class MongoCategoryData
 {
    private readonly IMongoCollection<CategoryModel> _categories;
    private readonly IMemoryCache _cache;
+   private const string cacheName = "CategoryData";
 
    public MongoCategoryData(IDbConnection db, IMemoryCache cache)
    {
       _cache = cache;
       _categories = db.CategoryCollection;
+   }
+
+   public async Task<List<CategoryModel>> GetAllCategories()
+   {
+      var output = _cache.Get<List<CategoryModel>>(cacheName);
+      if (output == null)
+      {
+         var results = await _categories.FindAsync(_ => true);
+         output = results.ToList();
+
+         _cache.Set(cacheName, output, TimeSpan.FromDays(value: 1));
+      }
+      return output;
    }
 }
