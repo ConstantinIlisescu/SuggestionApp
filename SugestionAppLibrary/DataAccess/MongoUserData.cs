34cfcd9e4c813ef4e9da793cbb8bbd027108735d
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-09-08 13:45:35 +0100

    Ask for user colection

---
+++ SugestionAppLibrary/DataAccess/MongoUserData.cs
@@ -0,0 +1,13 @@
+﻿
+using SuggestionAppLibrary.Models;
+
+namespace SuggestionAppLibrary.DataAccess;
+public class MongoUserData
+{
+	private readonly IMongoCollection<UserModel> _users;
+	public MongoUserData(IDbConnection db)
+	{
+		_users = db.UserCollection;
+	}
+
+}
