f0c059e8e9856dc7210c8f777bc3fdae0889e772
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-09-08 12:37:08 +0100

    Add properties to reference the colections through the aplication

--- SugestionAppLibrary/DataAccess/DbConnection.cs
+++ SugestionAppLibrary/DataAccess/DbConnection.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
 
 namespace SuggestionAppLibrary.DataAccess;
 public class DbConnection
 {
 
 	private readonly IConfiguration _config;
+	private readonly IMongoDatabase _db;
+	private string _conectionId = "MongoDB";
+	public string DbName { get; private set; }
+	public string CategoryCollectionName { get; private set; } = "categories";
+	public string StatusCollectionName { get; private set; } = "statuses";
+	public string UserColectionName { get; private set; } = "users";
+	public string SuggestionCollectionName { get; private set; } = "suggestions";
+
 
 	public DbConnection(IConfiguration config)
 	{
