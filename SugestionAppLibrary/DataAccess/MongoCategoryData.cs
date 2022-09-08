b86d4f3bab4e1dbd32ea7f7cb840387ab2e5ee02
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-09-08 14:20:33 +0100

    Add MongoCategoryData constructor

--- SugestionAppLibrary/DataAccess/MongoCategoryData.cs
+++ SugestionAppLibrary/DataAccess/MongoCategoryData.cs
@@ -1,5 +1,15 @@
-﻿namespace SuggestionAppLibrary.DataAccess;
+﻿using Microsoft.Extensions.Caching.Memory;
+using SuggestionAppLibrary.Models;
+
+namespace SuggestionAppLibrary.DataAccess;
 public class MongoCategoryData
 {
+   private readonly IMongoCollection<CategoryModel> _categories;
+   private readonly IMemoryCache _cache;
 
+   public MongoCategoryData(IDbConnection db, IMemoryCache cache)
+   {
+      _cache = cache;
+      _categories = db.CategoryCollection;
+   }
 }
